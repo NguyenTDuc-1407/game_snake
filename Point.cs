c8087f1c3eab91faa55e6dfb17a787c311764f8a
Duc.NT <311020ntd@gmail>
2024-09-09 20:44:14 +0700

    update

---
+++ Point.cs
@@ -0,0 +1,10 @@
+public class Point
+{
+    public int X { set; get; }
+    public int Y { set; get; }
+    public Point(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Thread _game = new Thread(SnakeControl.ListenKey);
+        _game.Start();
+        do
+        {
+            Console.Clear();
+            SnakeControl.Drawboard();
+            SnakeControl.setUpBoard();
+            SnakeControl.MoveSnakeHead();
+            SnakeControl.EatFood();
+            SnakeControl.SpawnBody();
+            SnakeControl.PopUpfood();
+            SnakeControl.ShowPoint();
+            // cài đặt time di chuyển
+            Task.Delay(SnakeControl.speed).Wait();
+
+        } while (true);
+
+    }
+
+}
